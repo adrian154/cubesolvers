978191d7757af135591ddaeaba88ff96467c59a1
adrian154 <94568a@gmail.com>
2022-11-08 23:33:40 -0800

    start coordinate stuff

--- main.c
+++ main.c
@@ -5,40 +5,6 @@
 
 #define TABLE_SIZE 88179840
 
-unsigned long long nodes = 0;
-
-void search(Cube *cube, int last_turn_face, int depth, uint8_t *table) {
-
-    if(depth == 11) {
-        return;
-    }
-
-    if(depth < 5)
-        printf("%llu nodes explored\n", nodes);
-
-    for(int face = 0; face < 6; face++) {
-
-        if(face == last_turn_face) continue;
-        if(last_turn_face == FACE_D && face == FACE_U ||
-           last_turn_face == FACE_R && face == FACE_L ||
-           last_turn_face == FACE_B && face == FACE_B) continue;
-        
-        for(int degree = 0; degree < 3; degree++) {
-            Cube next = *cube;
-            do_move(&next, face, degree);
-            nodes++;
-            int coord = get_corner_coordinate(&next);
-            if(table[coord] <= depth) {
-                continue;
-            }
-            table[coord] = depth;
-            search(&next, face, depth + 1, table);
-        }
-
-    }
-
-}
-
 void generate_pruning_table() {
 
     uint8_t *table = malloc(TABLE_SIZE);
@@ -46,8 +12,66 @@ void generate_pruning_table() {
         table[i] = 0xff;
     }
 
-    Cube cube = create_solved_cube();
-    search(&cube, -1, 0, table);
+    QueueNode *initial = malloc(sizeof(QueueNode));
+    initial->cube = create_solved_cube();
+    initial->last_turn_face = -1;
+    initial->depth = 0;
+    initial->next = NULL;
+
+
+    QueueNode *head = initial, *tail = initial;
+
+    // keep track of the maximum depth reached for logging
+    int depth_reached = 0;
+
+    do {
+
+        int cur_depth = head->depth + 1;
+        if(cur_depth > depth_reached) {
+            printf("reached depth %d\n", cur_depth);
+            depth_reached = cur_depth;
+        }
+
+        for(int face = 0; face < 6; face++) {
+
+            if(head->last_turn_face == face ||
+               head->last_turn_face == FACE_D && face == FACE_U ||
+               head->last_turn_face == FACE_R && face == FACE_L ||
+               head->last_turn_face == FACE_B && face == FACE_F)
+                continue;
+
+            for(int degree = 0; degree < 3; degree++) {
+                
+                Cube cube = head->cube;
+                do_move(&cube, face, degree);
+
+                // if we've seen this state already, ignore it 
+                int coord = get_corner_coordinate(&cube);
+                if(table[coord] <= cur_depth) {
+                    continue;
+                }
+
+                table[coord] = cur_depth;
+                
+                // enqueue the state
+                QueueNode *new = malloc(sizeof(QueueNode));
+                new->cube = cube;
+                new->last_turn_face = face;
+                new->depth = cur_depth;
+                new->next = NULL;
+
+                tail->next = new;
+                tail = new;
+
+            }
+        }
+
+        // dequeue
+        void *old_head = head;
+        head = head->next;
+        free(old_head);
+
+    } while(head != NULL);
 
     FILE *fp = fopen("corners.prune", "wb");
     fwrite(table, 1, TABLE_SIZE, fp);
---
+++ src/coordinates.h
@@ -0,0 +1,15 @@
+#ifndef __COORDINATES_H
+#define __COORDINATES_H
+
+/*
+ * The Rubik's Cube group, usually just referred to as the cube group or G,
+ * consists of the identity permutation and six face quarter turns
+ * <L,R,F,B,U,D>. The group operation is the composition of permutations (i.e.
+ * applying a sequence of moves to a cube state).
+ * 
+ * Consider the subgroup of the cube group where all corners are solved. There
+ * are 8!*3^7 possible corner orientations and permutations, so the number of
+ * cube states where all corners are solved is equal to |G|/(8!*3^7). 
+ */
+
+#endif
